015be1caa489beef2afef11e4160256b1de6290b
fbiayna <67982134+fbiayna@users.noreply.github.com>
2024-09-11 23:13:40 +0200

    Added fonts and color palette

---
+++ src/presentation/utils/colors/palette.ts
@@ -0,0 +1,13 @@
+export const ColorsPalette = {
+  skyBlue: "rgb(135, 181, 223)", // Light Sky Blue
+  softOrange: "rgb(250, 169, 67)", // Soft Tangerine
+  burntOrange: "rgb(197, 81, 34)", // Burnt Sienna
+  mutedOlive: "rgb(154, 172, 125)", // Muted Olive
+  slateGray: "rgb(118, 133, 164)", // Slate Blue-Gray
+  deepIndigo: "rgb(51, 51, 102)", // Deep Indigo
+  coralPink: "rgb(237, 159, 142)", // Coral Pink
+  goldenAmber: "rgb(229, 149, 26)", // Golden Amber
+  forestGreen: "rgb(68, 95, 25)", // Forest Green
+  black: "rgb(0, 0, 0)", // Black
+  white: "rgb(255, 255, 255)", // White
+};
